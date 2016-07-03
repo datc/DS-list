15d23b81ec049ee9a44c2a6ea9e7a984f60862ce
Ubuntu <ubuntu@ip-10-23-69-140.cn-north-1.compute.internal>
2016-07-03 05:16:31 +0000

    cgo

--- stack_c/main.go
+++ stack_c/main.go
@@ -12,16 +12,9 @@ import (
 func main() {
 	var Stk C.struct_SqStack
 	C.stack()
-	// C.InitStack(Stk)
+ C.InitStack(Stk)
 	fmt.Println(Stk)
 }
 
 func test() {
-	type p C.struct_Person
-	person := p{C.CString("Giorgis"), 30, 6, 175}
-	fmt.Println(person)
-	fmt.Println(C.GoString(person.name))
-	fmt.Println(person.age)
-	fmt.Println(person.height)
-	fmt.Println(person.weight)
 }
--- stack_c/stack_c.c
+++ stack_c/stack_c.c
@@ -1,16 +1,16 @@
 #include "stdio.h"
-// #include "stack_c.h"
+ #include "stack_c.h"
 
 void stack(){
 	printf("%s\n--STACK", "stack");
 }
 
-// void InitStack(struct SqStack s){
-// 	s.stacksize = 0;
+ void InitStack(struct SqStack s){
+ 	s.stacksize = 0;
 // 	s.top = NULL;
 // 	s.base = NULL;
 // 	// return 1;
-// }
+ }
 
 // int main(int argc, char const *argv[])
 // {
