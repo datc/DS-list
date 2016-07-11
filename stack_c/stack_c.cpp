63d0501f9f87b4c47b2f5fbd8e6e696e427c277e
shaalx <shaalx@yahoo.com.sg>
2016-07-11 21:08:52 +0800

    auto

--- stack_c/stack_c.cpp
+++ stack_c/stack_c.cpp
@@ -12,33 +12,48 @@ struct Stack
 	int size;
 };
 
+/*
+* 展示栈内容
+*/
 extern void Display(struct Stack stk);
+
+/*
+* 判断栈顶元素距离栈底高度，Go不能做指针运算，需要提供该函数
+*/
 int before(int * a,int* b){
 	return a - b;
 }
 
+/*
+* Go 不能直接索引，需要提供该函数
+*/
 int index(int * base,int length){
 	return base[length];
 }
 
+// 初始化
 void Init(struct Stack * stk,int size){
 	stk->base = new int[size];
 	stk->top = stk->base;
 	stk->size = size;
 }
 
+// 栈容量
 int Size(Stack stk){
 	return stk.size;
 }
 
+// 栈大小
 int Length(struct Stack stk){
 	return before(stk.top,stk.base);
 }
 
+// 是否为空
 bool IsEmpty(struct Stack stk){
 	return Length(stk)<=0;
 }
 
+// 是否满栈
 bool IsFull(struct Stack stk){
 	if (Length(stk) >= Size(stk))
 	{
@@ -47,6 +62,7 @@ bool IsFull(struct Stack stk){
 	return false;
 }
 
+// 压栈
 bool Push(struct Stack * stk,int v){
 	if (IsFull(*stk))
 	{
@@ -57,6 +73,7 @@ bool Push(struct Stack * stk,int v){
 	return true;
 }
 
+// 弹栈
 int Pop(struct Stack * stk,bool * ok){
 	if (IsEmpty(*stk))
 	{
@@ -68,6 +85,7 @@ int Pop(struct Stack * stk,bool * ok){
 	return *(stk->top);
 }
 
+// 查看栈顶元素
 int Seek(struct Stack stk,bool * ok){
 if (IsEmpty(stk))
 	{
