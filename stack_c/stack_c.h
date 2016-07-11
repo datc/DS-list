32fbf3b95f2b193c773873eb6a7ffebb3ddf802a
shaalx <shaalx@yahoo.com.sg>
2016-07-11 21:10:31 +0800

    auto

--- stack_c/stack_c.h
+++ stack_c/stack_c.h
@@ -15,18 +15,18 @@ struct Stack
 	int size;
 };
 
-extern void Display(struct Stack stk);
+void Display(struct Stack stk);
 int before(int * a,int* b);
-extern int index(int * base,int length);
+int index(int * base,int length);
   
-extern void Init(struct Stack * stk,int size);
-extern int Size(struct Stack stk);
-extern int Length(struct Stack stk);
-extern bool IsEmpty(struct Stack stk);
-extern bool IsFull(struct Stack stk);
-extern bool Push(struct Stack * stk,int v);
-extern int Pop(struct Stack * stk,bool * ok);
-extern int Seek(struct Stack stk,bool * ok);
+void Init(struct Stack * stk,int size);
+int Size(struct Stack stk);
+int Length(struct Stack stk);
+bool IsEmpty(struct Stack stk);
+bool IsFull(struct Stack stk);
+bool Push(struct Stack * stk,int v);
+int Pop(struct Stack * stk,bool * ok);
+int Seek(struct Stack stk,bool * ok);
 
 #ifdef __cplusplus  
 }  
